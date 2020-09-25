59383f37e3a0e43c274ed82c05d96e84b0281197
Roman <romaha48915@gmail.com>
2020-09-25 21:39:02 +0300

    add tableView in BreedsVC

--- Cats/Breeds Controller/BreedsViewController.swift
+++ Cats/Breeds Controller/BreedsViewController.swift
@@ -10,10 +10,55 @@ import UIKit
 
 class BreedsViewController: UIViewController {
 
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
+    
+    
+    // MARK:- Override viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
-                        	
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupTableView()
+        
+    }
+    
+    fileprivate func setupTableView(){
+        view.addSubview(tableView)
+        
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
     }
     
 }
+
+extension BreedsViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+       
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+    
+    
+
