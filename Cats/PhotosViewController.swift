e317b90964ab03510c6a11ef47339e3a123a4562
Roman <romaha48915@gmail.com>
2020-09-26 21:59:47 +0300

    add labels and methdos to PhotosVC

--- Cats/PhotosViewController.swift
+++ Cats/PhotosViewController.swift
@@ -9,14 +9,120 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK:- UI objects
+    let buttonCat: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(#imageLiteral(resourceName: "cat"), for: .normal)
+        return button
+    }()
+    
+    let labelRandomCat: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Random photos cats"
+        label.font = UIFont(name: "Chalkduster", size: 27)
+        return label
+    }()
+    
+    let labelTap: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Сlick on the photo \nof the cat"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = UIFont(name: "Chalkduster", size: 27)
+        return label
+    }()
+    
+    // MARK:- NetworkManager
+    var networkManager = NetworkCatsManager()
+    
+    // MARK:- class fields
+    let limitDownloadImage = 10
+    var photosURL = [ImageCat]()
+    var indexImage = 0
+    
+    // MARK:- Override loadView
+    override func loadView() {
+        super.loadView()
+     setupButtonAndLabels()
     }
     
-
+    // MARK:- Override viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        
+    }
     
-
+    // MARK:- Override viewWillAppear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.labelTap.pulsateStartAnimation()
+    }
+    
+    // MARK:- method setupUI components
+    fileprivate func setupUI(){
+        view.backgroundColor = .systemBackground
+        buttonCat.isHidden = true
+        labelIsHidden(true)
+        
+        loadImages()
+    }
+    
+    // MARK:- method hide UI
+    fileprivate func labelIsHidden(_ bool: Bool){
+        self.buttonCat.isHidden = bool
+        self.labelRandomCat.isHidden = bool
+        self.labelTap.isHidden = bool
+    }
+    
+    // MARK:- method setupButtonAndLabels
+    fileprivate func setupButtonAndLabels(){
+        view.addSubview(buttonCat)
+        view.addSubview(labelRandomCat)
+        view.addSubview(labelTap)
+        
+        buttonCat.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonCat.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        buttonCat.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        buttonCat.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7).isActive = true
+        buttonCat.addTarget(self, action: #selector(self.buttonCatTapped(sender:)), for: .touchUpInside)
+        
+        labelRandomCat.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelRandomCat.bottomAnchor.constraint(equalTo: buttonCat.topAnchor,constant: -50).isActive = true
+        
+        labelTap.topAnchor.constraint(equalTo: buttonCat.bottomAnchor,constant: 50).isActive = true
+        labelTap.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: 16).isActive = true
+        labelTap.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16).isActive = true
+    }
+    
+    // MARK:- method buttonCatTapped
+    @objc fileprivate func buttonCatTapped(sender: UIButton){
+      labelTap.layer.removeAllAnimations()
+        sender.rotate()
+        sender.imageURL(imageURL: photosURL[indexImage].url,label: labelTap)
+        
+        indexImage += 1
+        if indexImage == limitDownloadImage{
+            indexImage = 0
+            setupUI()
+        }
+    }
+    // MARK:- request images
+    fileprivate func loadImages(){
+        
+        networkManager.onCompletionImages = { images in
+            self.photosURL = images
+            
+            DispatchQueue.main.async {
+                self.labelIsHidden(false)
+                self.labelTap.pulsateStartAnimation()
+            }
+        }
+        networkManager.fetchImagesCats(cat: nil, limit: limitDownloadImage)
+    }
 }
+
