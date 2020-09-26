a205bcd937c64ac5bedeb622fd873cd44c3ed29f
Roman <romaha48915@gmail.com>
2020-09-26 16:45:03 +0300

    add Model ImageCat

---
+++ Cats/ImageCat.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCat.swift
+//  Cats
+//
+//  Created by Romko on 26.09.2020.
+//  Copyright © 2020 Romko. All rights reserved.
+//
+
+import Foundation
+
+struct ImageCat: Decodable {
+    let url: String?
+}
